b28fa6e12f5fa4499d763ada69223325525f4b54
Jeongseok Lee <jeongseoklee@MacBook-Pro-2.local>
2026-07-06 00:43:19 +0900

    auto: update IntroSection.tsx [2026-07-06 00:43:19]

--- app/components/IntroSection.tsx
+++ app/components/IntroSection.tsx
@@ -28,7 +28,7 @@ const toolGroups = [
     ],
   },
 ];
-const techniques = ['촬영', '편집', '기획', '자막', '모션그래픽', '라이브 송출'];
+const techniques = ['기획', '촬영', '편집', '자막', '모션그래픽', '라이브 송출'];
 
 const displayFont = 'var(--font-display-stack)';
 
